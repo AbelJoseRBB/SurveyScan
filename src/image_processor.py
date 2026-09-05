e9a6d431b23075073ebf94ec902a903f73d8c545
AbelJoseRBB <abeljose018@gmail.com>
2026-09-05 14:28:00 -0300

    Feat: Processamento de pdf e imagem

---
+++ src/image_processor.py
@@ -0,0 +1,76 @@
+from pathlib import Path
+import cv2 as cv
+import numpy as np 
+import pymupdf 
+import sys
+
+
+def pdfToImage(pdf_path: str, dpi: int = 300) -> list[np.ndarray]:
+    doc =  pymupdf.open(pdf_path)
+    imgs = []
+
+    for page in doc:
+        pixmap = page.get_pixmap(dpi=dpi)
+
+        image = np.frombuffer(
+            pixmap.samples,
+            dtype=np.uint8
+        )
+
+        image = image.reshape(
+            pixmap.height,
+            pixmap.width,
+            pixmap.n
+        )
+
+        # PyMuPDF fornece RGB/RGBA, enquanto OpenCV trabalha com BGR.
+        if pixmap.n == 4:
+            image = cv.cvtColor(image, cv.COLOR_RGBA2BGR)
+        else:
+            image = cv.cvtColor(image, cv.COLOR_RGB2BGR)
+
+        imgs.append(image)
+
+    doc.close()
+
+    return imgs
+
+
+
+def loadImage(image_path: str) -> np.ndarray:
+    img = cv.imread(image_path)
+
+    if img is None:
+        sys.exit(f"ERROR: Could not read the image: {image_path}.")
+
+    return img
+
+
+
+def preprocessImage(image: np.ndarray) -> np.ndarray:
+    # conversão da imagem para cinza
+    gray_img = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+
+    # binariza a imagem
+    binary = cv.threshold(
+        gray_img,
+        0,
+        255,
+        cv.THRESH_BINARY_INV + cv.THRESH_OTSU
+    )[1]
+
+    return binary
+
+
+def saveProcessedImage(image: np.ndarray, output_path: str) -> None:
+    output = Path(output_path)
+
+    output.parent.mkdir(
+        parents= True,
+        exist_ok= True
+    )
+
+    cv.imwrite(str(output), image)
+
+    
+
--- src/main.py
+++ src/main.py
@@ -1,13 +1,26 @@
 # Ativar venv
 # .\venv\Scripts\Activate.ps1 
 
-from preprocess import preprocess_image
-from answer_reader import read_answers
-from age_reader import read_age
-from excel_exporter import export_results
+from image_processor import preprocessImage, saveProcessedImage, pdfToImage
 
 def main():
-    pass
+    pages = pdfToImage(
+        "formularios/Forms.pdf"
+    )
+
+    print(f"Paginas encontradas: {len(pages)}")
+
+
+    for i, page in enumerate(pages, start=1):
+
+        processed = preprocessImage(page)
+
+        saveProcessedImage(
+            processed,
+            f"output/page{i}_processed.png"
+        )
+
+        print(f"Pagina {i} processada.") 
 
 if __name__ == "__main__":
     main()
